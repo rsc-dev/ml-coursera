1f9e6442e9a1010081b49c7e7b178a5413a4af4c
rsc-dev <roscoe@o2.pl>
2018-02-05 17:45:20 +0100

    Implemented Select Threshold.

--- ex8/code/selectThreshold.m
+++ ex8/code/selectThreshold.m
@@ -23,7 +23,21 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-
+    predictions = (pval < epsilon);
+    
+    tp = sum((predictions == 1) & (yval == 1));
+    fp = sum((predictions == 1) & (yval == 0));
+    fn = sum((predictions == 0) & (yval == 1));
+    
+    prec = tp / (tp + fp);
+    recal = tp / (tp + fn);
+    
+    F1 = (2 * prec * recal) / (prec + recal);
+    
+    if (F1 > bestF1)
+        bestEpsilon = epsilon;
+        bestF1 = F1;
+    endif
 
 
 
